538e22f331d8256818da9a41856bfda3c61301e8
Alex <MetalGear753@gmail.com>
2016-04-07 19:37:27 -0400

    Cleaned up the display of coordinates a bit.

--- ServerMap_0.1.1/control.lua
+++ ServerMap_0.1.1/control.lua
@@ -2,6 +2,19 @@ require "util"
 require "defines"
 
 script.on_event(defines.events.on_tick, function(event)
-	game.player.print(game.player.surface.name)
-	game.player.print(game.player.position["x"] .. ", " .. game.player.position["y"])
+	if counter == nil then
+		counter = 0
+	end
+	
+	if game.player.gui.top.mapCoord == nil then
+		game.player.gui.top.add{type="label", name="mapCoord", caption="0, 0"}
+	end
+		
+    if counter % 10 == 0 then
+		game.player.gui.top.mapCoord.caption = math.floor(game.player.position["x"]) .. ", " .. math.floor(game.player.position["y"])
+		--game.player.print(game.player.surface.name)
+		--game.player.print(math.floor(game.player.position["x"]) .. ", " .. math.floor(game.player.position["y"]))
+	end
+	
+	counter = counter + 1
 end)
