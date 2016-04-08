6ffd7ad59e3fc8114116d266e5292a9b602da998
Alex <MetalGear753@gmail.com>
2016-04-08 14:39:13 -0400

    Reduced the frequency that the calculations update. Added entity printing and tile printing.

--- ServerMap_0.1.1/control.lua
+++ ServerMap_0.1.1/control.lua
@@ -2,19 +2,38 @@ require "util"
 require "defines"
 
 script.on_event(defines.events.on_tick, function(event)
-	if counter == nil then
-		counter = 0
-	end
+	
+	counter = game.tick
 	
 	if game.player.gui.top.mapCoord == nil then
 		game.player.gui.top.add{type="label", name="mapCoord", caption="0, 0"}
+		game.player.gui.left.add{type="label", name="tileName", caption="Tile: "}
 	end
 		
-    if counter % 10 == 0 then
-		game.player.gui.top.mapCoord.caption = math.floor(game.player.position["x"]) .. ", " .. math.floor(game.player.position["y"])
+	if counter % 10 == 0 then
+		printCoord()
+		checkItem()
 		--game.player.print(game.player.surface.name)
 		--game.player.print(math.floor(game.player.position["x"]) .. ", " .. math.floor(game.player.position["y"]))
 	end
-	
-	counter = counter + 1
+
 end)
+
+function printCoord()
+	game.player.gui.top.mapCoord.caption = math.floor(game.player.position["x"]) .. ", " .. math.floor(game.player.position["y"]) .. " Tick: " .. game.tick
+end
+
+function checkItem()
+
+	entityName = game.surfaces["nauvis"].find_entities{{math.floor(game.player.position["x"]), math.floor(game.player.position["y"])}, {math.floor(game.player.position["x"]) + 1, math.floor(game.player.position["y"]) + 1}}
+	
+	if entityName then
+		if entityName[2] then
+			enName = entityName[2].name
+		else
+			enName = ""
+		end
+		game.player.gui.left.tileName.caption = " Tile: " .. game.surfaces["nauvis"].get_tile(game.player.position["x"], game.player.position["y"]).name .. " --- Entity: " .. enName
+	end
+
+end
